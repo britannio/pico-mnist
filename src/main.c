2880a5f0cc8d06f13b71ef38a6f77f865102a71f
Britannio <britanniojarrett@gmail.com>
2024-03-14 17:55:31 +0000

    figured out image resizing formula

--- src/main.c
+++ src/main.c
@@ -9,8 +9,10 @@
 #include "hardware/vreg.h"
 
 // Core 0
-volatile uint8_t core_0_image_buf[324 * 324];
+uint8_t core_0_image_buf[324 * 324];
 uint8_t prediction_img_buf[28 * 28 * 2];
+int8_t mnist_prediction = -1;
+
 
 // Core 1
 uint8_t core_1_image_buf[324 * 324];
@@ -20,6 +22,9 @@ uint8_t imageFeedBuf[80 * 80 * 2];
 uint8_t header[2] = {0x55, 0xAA};
 
 #define FLAG_VALUE 123
+
+#define FLAG_COPY_IMG 0
+#define FLAG_DISPLAY_RES 1
 
 void create_arducam_config(struct arducam_config *config) {
     config->sccb = i2c0;
@@ -65,12 +70,14 @@ void core1_entry() {
         // Writes to core_1_image_buf
         arducam_capture_frame(&config);
 
+        bool display_result = false;
+
         // Multicore
         if (multicore_fifo_rvalid()) {
-            printf("SENT COPY\n");
+            multicore_fifo_pop_blocking();
             // Core 0 would like us to make a copy of core_1_image_buf that it can safely read
             memcpy(&core_0_image_buf, &core_1_image_buf, sizeof(core_1_image_buf));
-            multicore_fifo_pop_blocking();
+            display_result = true;
         }
 
         // Read from the 324x324 frame and fill imageFeedBuf with a 80x80 RBG copy of the image.
@@ -87,6 +94,15 @@ void core1_entry() {
 
         // Display the image feed.
         ST7735_DrawImage(0, 0, 80, 80, imageFeedBuf);
+
+        // TODO Display the RGB image LTWH(6,26,28,28)
+        // TODO Display the prediction (x>=40...)
+        if (display_result) {
+            // Display the prediction image
+            ST7735_DrawImage(6, 80 + 26, 28, 28, prediction_img_buf);
+            // Display the current prediction
+            ST7735_FillRectangle(80 - 6 - 28, 80 + 26, 28, 28, 0XC0);
+        }
     }
 }
 
@@ -95,19 +111,39 @@ void core0_entry() {
         // Request a copy of the image from core 1.
         // Once it completes, the full size image will be available in core_0_image_buf
         // This is not time sensitive unlike the realtime updates to the camera feed on core 1.
-//        multicore_fifo_push_blocking(0);
+        multicore_fifo_push_blocking(0);
 
-//        core_0_image_buf
 
 
         // TODO Resize to 28x28 RGB to be displayed
 //        prediction_img_buf
-//        int index = 0;
-//        for (int x = 0; x < 28; x++) {
-//            for (int y = 0; x < 28; x++) {
-//                long i = (2 + (56 * 2) - 2 * y) * 324 + (2 + 28 + 2 * x);
+        int index = 0;
+
+        for (int y = 0; y < 28; y++) {
+            for (int x = 0; x < 28; x++) {
+
+//                long i = (2 + 56 - 2 * y) * 324 + (2 + 14 + 2 * x);
+                int x_scale_factor = 11;
+                int y_scale_factor = 11;
+                long i = (y_scale_factor + (28 * y_scale_factor) - y_scale_factor * y) * 324 + (x_scale_factor + (28 * x_scale_factor) + x_scale_factor * x);
+                uint8_t c = core_0_image_buf[i];
+                uint16_t imageRGB = ST7735_COLOR565(c, c, c);
+                prediction_img_buf[index++] = (uint8_t) (imageRGB >> 8) & 0xFF;
+                prediction_img_buf[index++] = (uint8_t) (imageRGB) & 0xFF;
+            }
+        }
+//        for (int y = 0; y < 28; y++) {
+//            for (int x = 0; x < 28; x++) {
+////                Reference
+////                long i = (2 + 160 - 2 * y) * 324 + (2 + 40 + 2 * x);
+//
+////                long i = (2 + (56 * 2) - 2 * y) * 324 + (2 + 28 + 2 * x);
+////                long i = (2 + 160 - 2 * y) * 324 + (2 + 40 + 2 * x);
+//
+//                int x_scaled = x * 11;
+//                int y_scaled = y * 11;
+//                long i = (324 * y_scaled) + x_scaled;
 //                uint8_t c = core_0_image_buf[i];
-////                resized[i] = (float) c / 255.0;
 //                uint16_t imageRGB = ST7735_COLOR565(c, c, c);
 //                prediction_img_buf[index++] = (uint8_t) (imageRGB >> 8) & 0xFF;
 //                prediction_img_buf[index++] = (uint8_t) (imageRGB) & 0xFF;
@@ -118,11 +154,6 @@ void core0_entry() {
         // TODO Resize to [28*28] 'tensor' to feed into `net()`
         // TODO Feed tensor into `net()` to get the prediction
         // TODO Print the prediction
-
-        // TODO Display the RGB image LTWH(6,26,28,28)
-//        ST7735_DrawImage(6, 80 + 26, 28, 28, prediction_img_buf);
-
-        // TODO Display the prediction (x>=40...)
     }
 }
 
