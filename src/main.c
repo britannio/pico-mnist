854f0d931e393c7c95cadfada2e60a0dcb081312
Britannio <britanniojarrett@gmail.com>
2024-03-14 13:38:59 +0000

    checkpoint

--- src/main.c
+++ src/main.c
@@ -5,21 +5,22 @@
 #include "arducam/arducam.h"
 #include "lib/st7735.h"
 #include "lib/fonts.h"
-uint8_t image_buf[324*324];
-uint8_t displayBuf[80*160*2];
-uint8_t header[2] = {0x55,0xAA};
+uint8_t image_buf[324 * 324];
+uint8_t displayBuf[80 * 160 * 2];
+uint8_t header[2] = {0x55, 0xAA};
 
 #define FLAG_VALUE 123
 
-void core1_entry() {
-        multicore_fifo_push_blocking(FLAG_VALUE);
+void core1_entry()
+{
+	multicore_fifo_push_blocking(FLAG_VALUE);
 
-        uint32_t g = multicore_fifo_pop_blocking();
+	uint32_t g = multicore_fifo_pop_blocking();
 
-        if (g != FLAG_VALUE)
-          printf("Hmm, that's not right on core 1!\n");
-        else
-          printf("It's all gone well on core 1!\n");
+	if (g != FLAG_VALUE)
+		printf("Hmm, that's not right on core 1!\n");
+	else
+		printf("It's all gone well on core 1!\n");
 
 	gpio_init(PIN_LED);
 	gpio_set_dir(PIN_LED, GPIO_OUT);
@@ -46,47 +47,59 @@ void core1_entry() {
 	config.image_buf_size = sizeof(image_buf);
 
 	arducam_init(&config);
-	while (true) {
-	  gpio_put(PIN_LED, !gpio_get(PIN_LED));
-	  arducam_capture_frame(&config);
+	while (true)
+	{
+		gpio_put(PIN_LED, !gpio_get(PIN_LED));
+		arducam_capture_frame(&config);
 
-	  uint16_t index = 0;
-	  for (int y = 0; y < 160; y++) {
-	    for (int x = 0; x < 80; x++) {
-              uint8_t c = image_buf[(2+320-2*y)*324+(2+40+2*x)];
-              uint16_t imageRGB   = ST7735_COLOR565(c, c, c);
-              displayBuf[index++] = (uint8_t)(imageRGB >> 8) & 0xFF;
-              displayBuf[index++] = (uint8_t)(imageRGB)&0xFF;
-            }
-	  }
-	  ST7735_DrawImage(0, 0, 80, 160, displayBuf);
+		uint16_t index = 0;
+		for (int y = 0; y < 80; y++)
+		{
+			for (int x = 0; x < 80; x++)
+			{
+				uint8_t c = image_buf[(2 + 160 - 2 * y) * 324 + (2 + 40 + 2 * x)];
+				uint16_t imageRGB = ST7735_COLOR565(c, c, c);
+				displayBuf[index++] = (uint8_t)(imageRGB >> 8) & 0xFF;
+				displayBuf[index++] = (uint8_t)(imageRGB) & 0xFF;
+			}
+		}
+		ST7735_DrawImage(0, 0, 80, 160, displayBuf);
 	}
 }
 
 #include "hardware/vreg.h"
 
-int main() {
-  int loops=20;
-  stdio_init_all();
-  while (!tud_cdc_connected()) { sleep_ms(100); if (--loops==0) break;  }
+int main()
+{
+	int loops = 20;
+	stdio_init_all();
+	while (!tud_cdc_connected())
+	{
+		sleep_ms(100);
+		if (--loops == 0)
+			break;
+	}
+	printf("loops(%d)\n", loops);
 
-  printf("tud_cdc_connected(%d)\n", tud_cdc_connected()?1:0);
+	printf("tud_cdc_connected(%d)\n", tud_cdc_connected() ? 1 : 0);
 
-  vreg_set_voltage(VREG_VOLTAGE_1_30);
-  sleep_ms(1000);
-  set_sys_clock_khz(250000, true);
+	vreg_set_voltage(VREG_VOLTAGE_1_30);
+	sleep_ms(1000);
+	// Overclock from 125Mhz to 250Mhz
+	set_sys_clock_khz(250000, true);
 
-  multicore_launch_core1(core1_entry);
+	multicore_launch_core1(core1_entry);
 
-  uint32_t g = multicore_fifo_pop_blocking();
+	uint32_t g = multicore_fifo_pop_blocking();
 
-  if (g != FLAG_VALUE)
-    printf("Hmm, that's not right on core 0!\n");
-  else {
-    multicore_fifo_push_blocking(FLAG_VALUE);
-    printf("It's all gone well on core 0!\n");
-  }
+	if (g != FLAG_VALUE)
+		printf("Hmm, that's not right on core 0!\n");
+	else
+	{
+		multicore_fifo_push_blocking(FLAG_VALUE);
+		printf("It's all gone well on core 0!\n");
+	}
 
-  while (1)
-    tight_loop_contents();
+	while (1)
+		tight_loop_contents();
 }
