02fc0fd23f00fe5a1cd1daffd2522e7108afa385
Britannio <britanniojarrett@gmail.com>
2024-03-14 13:39:17 +0000

    camera stream + image copying

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 `PICO_SDK_PATH` should be set as an environment variable. It should point to the root of the repo https://github.com/raspberrypi/pico-sdk.git once cloned.
 
+`cd build/` then `cmake .. && make -j8` to compile
--- src/main.c
+++ src/main.c
@@ -5,101 +5,155 @@
 #include "arducam/arducam.h"
 #include "lib/st7735.h"
 #include "lib/fonts.h"
-uint8_t image_buf[324 * 324];
-uint8_t displayBuf[80 * 160 * 2];
+#include "lib/mnist.h"
+#include "hardware/vreg.h"
+
+// Core 0
+volatile uint8_t core_0_image_buf[324 * 324];
+uint8_t prediction_img_buf[28 * 28 * 2];
+
+// Core 1
+uint8_t core_1_image_buf[324 * 324];
+uint8_t imageFeedBuf[80 * 80 * 2];
+
+
 uint8_t header[2] = {0x55, 0xAA};
 
 #define FLAG_VALUE 123
 
-void core1_entry()
-{
-	multicore_fifo_push_blocking(FLAG_VALUE);
+void create_arducam_config(struct arducam_config *config) {
+    config->sccb = i2c0;
+    config->sccb_mode = I2C_MODE_16_8;
+    config->sensor_address = 0x24;
+    config->pin_sioc = PIN_CAM_SIOC;
+    config->pin_siod = PIN_CAM_SIOD;
+    config->pin_resetb = PIN_CAM_RESETB;
+    config->pin_xclk = PIN_CAM_XCLK;
+    config->pin_vsync = PIN_CAM_VSYNC;
+    config->pin_y2_pio_base = PIN_CAM_Y2_PIO_BASE;
 
-	uint32_t g = multicore_fifo_pop_blocking();
+    config->pio = pio0;
+    config->pio_sm = 0;
 
-	if (g != FLAG_VALUE)
-		printf("Hmm, that's not right on core 1!\n");
-	else
-		printf("It's all gone well on core 1!\n");
-
-	gpio_init(PIN_LED);
-	gpio_set_dir(PIN_LED, GPIO_OUT);
-
-	ST7735_Init();
-	ST7735_DrawImage(0, 0, 80, 160, arducam_logo);
-
-	struct arducam_config config;
-	config.sccb = i2c0;
-	config.sccb_mode = I2C_MODE_16_8;
-	config.sensor_address = 0x24;
-	config.pin_sioc = PIN_CAM_SIOC;
-	config.pin_siod = PIN_CAM_SIOD;
-	config.pin_resetb = PIN_CAM_RESETB;
-	config.pin_xclk = PIN_CAM_XCLK;
-	config.pin_vsync = PIN_CAM_VSYNC;
-	config.pin_y2_pio_base = PIN_CAM_Y2_PIO_BASE;
-
-	config.pio = pio0;
-	config.pio_sm = 0;
-
-	config.dma_channel = 0;
-	config.image_buf = image_buf;
-	config.image_buf_size = sizeof(image_buf);
-
-	arducam_init(&config);
-	while (true)
-	{
-		gpio_put(PIN_LED, !gpio_get(PIN_LED));
-		arducam_capture_frame(&config);
-
-		uint16_t index = 0;
-		for (int y = 0; y < 80; y++)
-		{
-			for (int x = 0; x < 80; x++)
-			{
-				uint8_t c = image_buf[(2 + 160 - 2 * y) * 324 + (2 + 40 + 2 * x)];
-				uint16_t imageRGB = ST7735_COLOR565(c, c, c);
-				displayBuf[index++] = (uint8_t)(imageRGB >> 8) & 0xFF;
-				displayBuf[index++] = (uint8_t)(imageRGB) & 0xFF;
-			}
-		}
-		ST7735_DrawImage(0, 0, 80, 160, displayBuf);
-	}
+    config->dma_channel = 0;
+    config->image_buf = core_1_image_buf;
+    config->image_buf_size = sizeof(core_1_image_buf);
 }
 
-#include "hardware/vreg.h"
+void core1_entry() {
+    // Initialisation
+    multicore_fifo_push_blocking(FLAG_VALUE);
+    uint32_t g = multicore_fifo_pop_blocking();
+    if (g != FLAG_VALUE) printf("Hmm, that's not right on core 1!\n");
+    else printf("It's all gone well on core 1!\n");
 
-int main()
-{
-	int loops = 20;
-	stdio_init_all();
-	while (!tud_cdc_connected())
-	{
-		sleep_ms(100);
-		if (--loops == 0)
-			break;
-	}
-	printf("loops(%d)\n", loops);
+    gpio_init(PIN_LED);
+    gpio_set_dir(PIN_LED, GPIO_OUT);
 
-	printf("tud_cdc_connected(%d)\n", tud_cdc_connected() ? 1 : 0);
+    ST7735_Init();
+    ST7735_DrawImage(0, 0, 80, 160, arducam_logo);
 
-	vreg_set_voltage(VREG_VOLTAGE_1_30);
-	sleep_ms(1000);
-	// Overclock from 125Mhz to 250Mhz
-	set_sys_clock_khz(250000, true);
+    struct arducam_config config;
+    create_arducam_config(&config);
+    arducam_init(&config);
 
-	multicore_launch_core1(core1_entry);
+    // Clear the screen
+    ST7735_FillScreen(0);
 
-	uint32_t g = multicore_fifo_pop_blocking();
+    while (true) {
+        gpio_put(PIN_LED, !gpio_get(PIN_LED));
+        // Writes to core_1_image_buf
+        arducam_capture_frame(&config);
 
-	if (g != FLAG_VALUE)
-		printf("Hmm, that's not right on core 0!\n");
-	else
-	{
-		multicore_fifo_push_blocking(FLAG_VALUE);
-		printf("It's all gone well on core 0!\n");
-	}
+        // Multicore
+        if (multicore_fifo_rvalid()) {
+            printf("SENT COPY\n");
+            // Core 0 would like us to make a copy of core_1_image_buf that it can safely read
+            memcpy(&core_0_image_buf, &core_1_image_buf, sizeof(core_1_image_buf));
+            multicore_fifo_pop_blocking();
+        }
 
-	while (1)
-		tight_loop_contents();
+        // Read from the 324x324 frame and fill imageFeedBuf with a 80x80 RBG copy of the image.
+        uint16_t index = 0;
+        for (int y = 0; y < 80; y++) {
+            for (int x = 0; x < 80; x++) {
+                long i = (2 + 160 - 2 * y) * 324 + (2 + 40 + 2 * x);
+                uint8_t c = core_1_image_buf[i];
+                uint16_t imageRGB = ST7735_COLOR565(c, c, c);
+                imageFeedBuf[index++] = (uint8_t) (imageRGB >> 8) & 0xFF;
+                imageFeedBuf[index++] = (uint8_t) (imageRGB) & 0xFF;
+            }
+        }
+
+        // Display the image feed.
+        ST7735_DrawImage(0, 0, 80, 80, imageFeedBuf);
+    }
+}
+
+void core0_entry() {
+    while (true) {
+        // Request a copy of the image from core 1.
+        // Once it completes, the full size image will be available in core_0_image_buf
+        // This is not time sensitive unlike the realtime updates to the camera feed on core 1.
+//        multicore_fifo_push_blocking(0);
+
+//        core_0_image_buf
+
+
+        // TODO Resize to 28x28 RGB to be displayed
+//        prediction_img_buf
+//        int index = 0;
+//        for (int x = 0; x < 28; x++) {
+//            for (int y = 0; x < 28; x++) {
+//                long i = (2 + (56 * 2) - 2 * y) * 324 + (2 + 28 + 2 * x);
+//                uint8_t c = core_0_image_buf[i];
+////                resized[i] = (float) c / 255.0;
+//                uint16_t imageRGB = ST7735_COLOR565(c, c, c);
+//                prediction_img_buf[index++] = (uint8_t) (imageRGB >> 8) & 0xFF;
+//                prediction_img_buf[index++] = (uint8_t) (imageRGB) & 0xFF;
+//            }
+//        }
+
+
+        // TODO Resize to [28*28] 'tensor' to feed into `net()`
+        // TODO Feed tensor into `net()` to get the prediction
+        // TODO Print the prediction
+
+        // TODO Display the RGB image LTWH(6,26,28,28)
+//        ST7735_DrawImage(6, 80 + 26, 28, 28, prediction_img_buf);
+
+        // TODO Display the prediction (x>=40...)
+    }
+}
+
+
+int main() {
+    int loops = 20;
+    stdio_init_all();
+    while (!tud_cdc_connected()) {
+        sleep_ms(100);
+        if (--loops == 0)
+            break;
+    }
+    printf("loops(%d)\n", loops);
+
+    printf("tud_cdc_connected(%d)\n", tud_cdc_connected() ? 1 : 0);
+
+    vreg_set_voltage(VREG_VOLTAGE_1_30);
+    sleep_ms(1000);
+    // Overclock from 125Mhz to 250Mhz
+    set_sys_clock_khz(250000, true);
+
+    multicore_launch_core1(core1_entry);
+
+    uint32_t g = multicore_fifo_pop_blocking();
+
+    if (g != FLAG_VALUE)
+        printf("Hmm, that's not right on core 0!\n");
+    else {
+        multicore_fifo_push_blocking(FLAG_VALUE);
+        printf("It's all gone well on core 0!\n");
+    }
+
+    core0_entry();
 }
